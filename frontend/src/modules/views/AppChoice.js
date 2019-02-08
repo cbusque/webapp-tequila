2f3e99904e6a5c961e95f2cf26ae99b18a61fc27
cbsuque <cedrick.busque@polymtl.ca>
2019-02-08 15:14:15 -0500

    add hooks to frontend

--- frontend/src/modules/views/AppChoice.js
+++ frontend/src/modules/views/AppChoice.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import ButtonBase from "@material-ui/core/ButtonBase";
 import Typography from "@material-ui/core/Typography";
 import axios from "axios";
+
+import image_lions from "../../static/images/lion.jpg";
 
 var page = 0;
 const styles = theme => ({
@@ -80,22 +82,22 @@ const styles = theme => ({
   }
 });
 
-const images = [
+const imagesAct1 = [
   {
-    url:
-      "https://previews.123rf.com/images/ajibon/ajibon1705/ajibon170500072/79522755-angry-lion-mascot-vector-illustration.jpg",
+    url: image_lions,
     title: "Relacher les lions",
     width: "20%"
   },
   {
-    url:
-      "https://st3.depositphotos.com/14490296/18838/v/1600/depositphotos_188388166-stock-illustration-realistic-gold-coin-explosion-or.jpg",
+    url: image_lions,
     title: "Faire tomber une cenne",
     width: "30%"
-  },
+  }
+];
+
+const imagesAct2 = [
   {
-    url:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWhcBlV1Ccs4B79A--7mnD8TnfINwNJu-QqXACiK8jawZJWIb9",
+    url: image_lions,
     title: "Ajouter un AI",
     width: "30%"
   }
@@ -104,9 +106,25 @@ const images = [
 function AppChoice(props) {
   const { classes } = props;
 
+  const [imageChoice, setActImages] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/api/options").then(Response => {
+      console.log(Response.data);
+      switch (Response.data) {
+        case "not_started":
+          setActImages(imagesAct1);
+          break;
+        case "act1":
+          setActImages(imagesAct2);
+          break;
+      }
+    });
+  }, []);
+
   return (
     <div className={classes.root}>
-      {images.map(image => (
+      {imageChoice.map(image => (
         <ButtonBase
           onClick={() => {
             console.log("onClick");
@@ -152,6 +170,57 @@ function AppChoice(props) {
     </div>
   );
 }
+// function AppChoice(props) {
+//   const { classes } = props;
+
+//   return (
+//     <div className={classes.root}>
+//       {imagesAct1.map(image => (
+//         <ButtonBase
+//           onClick={() => {
+//             console.log("onClick");
+//             axios
+//               .get(
+//                 "http://localhost:3001/api/actions/action?action=" + image.title
+//               )
+//               .then(console.log("DONE"))
+//               .then(page++);
+//             if (page >= 8) {
+//               window.location.reload();
+//               page = 0;
+//             }
+//           }}
+//           focusRipple
+//           key={image.title}
+//           className={classes.image}
+//           focusVisibleClassName={classes.focusVisible}
+//           style={{
+//             width: image.width
+//           }}
+//         >
+//           <span
+//             className={classes.imageSrc}
+//             style={{
+//               backgroundImage: `url(${image.url})`
+//             }}
+//           />
+//           <span className={classes.imageBackdrop} />
+//           <span className={classes.imageButton}>
+//             <Typography
+//               component="span"
+//               variant="subtitle1"
+//               color="inherit"
+//               className={classes.imageTitle}
+//             >
+//               {image.title}
+//               <span className={classes.imageMarked} />
+//             </Typography>
+//           </span>
+//         </ButtonBase>
+//       ))}
+//     </div>
+//   );
+// }
 
 AppChoice.propTypes = {
   classes: PropTypes.object.isRequired
